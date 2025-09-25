fd1a70e9f7af55809cb06f797f41d75fd63e42af
Apon Talukder <apontalukder212@gmail.com>
2025-09-26 00:50:47 +0600

    responsive carts

--- src/Components/main/Main.jsx
+++ src/Components/main/Main.jsx
@@ -5,8 +5,8 @@ const Main = () => {
   return (
     <>
       <div className="container mx-auto">
-        <div className="grid grid-cols-2 gap-[20px] my-[50px] p-5">
-          <div className=" bg-[linear-gradient(125.07deg,rgba(99,46,227,1),rgba(159,98,242,1)_100%)] w-full h-64 box  ">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-[20px] my-[50px] p-5">
+          <div className=" bg-[linear-gradient(125.07deg,rgba(99,46,227,1),rgba(159,98,242,1)_100%)] w-full h-60 rounded-md  ">
             <div className="flex justify-between">
               <div>
                 <img className="md:block hidden" src={bg1} alt="" />
@@ -20,13 +20,13 @@ const Main = () => {
               </div>
             </div>
           </div>
-          <div className=" bg-[linear-gradient(90deg,rgba(84,207,104,1),rgba(0,130,122,1)_100%)] w-full h-64  box">
+          <div className=" bg-[linear-gradient(90deg,rgba(84,207,104,1),rgba(0,130,122,1)_100%)] w-full h-60    rounded-md">
             <div className="flex justify-between">
               <div>
                 <img className="md:block hidden" src={bg1} alt="" />
               </div>
               <div className="text-stone-200 h-[200px] flex flex-col items-center justify-center">
-                <h1 className="font-bold md:text-[35px] ">Resolved</h1>
+                <h1 className="font-bold md:text-[20px] ">Resolved</h1>
                 <p className="font-semibold md:text-3xl">0</p>
               </div>
               <div>
